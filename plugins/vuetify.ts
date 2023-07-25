1b93c6ca33857f550ad8c11ce189fd79019a1db0
shineuiseong <30334829+shineuiseong@users.noreply.github.com>
2023-07-25 22:58:19 +0900

    feature(plugins/vuetify): config vuetify

---
+++ plugins/vuetify.ts
@@ -0,0 +1,40 @@
+import { createVuetify, ThemeDefinition } from 'vuetify'
+import { aliases, mdi } from 'vuetify/iconsets/mdi'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+const LightTheme: ThemeDefinition = {
+  variables: {},
+  colors: {
+    primary: '#4376fb',
+    accent: '#757575',
+    secondary: '#FF8F00',
+    info: '#26A69A',
+    warning: '#FFC107',
+    error: '#bb252a',
+    success: '#4CAF50'
+  }
+}
+
+export default defineNuxtPlugin((NuxtApp) => {
+  const vuetify = createVuetify({
+    components,
+    directives,
+    ssr: true,
+    theme: {
+      themes: {
+        light: LightTheme,
+        variables: {}
+      }
+    },
+    icons: {
+      defaultSet: 'mdi',
+      aliases,
+      sets: {
+        mdi
+      }
+    }
+  })
+
+  NuxtApp.vueApp.use(vuetify)
+})
