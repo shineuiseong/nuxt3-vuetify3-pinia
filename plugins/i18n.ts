050fd9101a2c5922e2b28cbbd581f43174d8b9fc
shineuiseong <crossstory12@gmail.com>
2023-07-31 16:44:55 +0900

    chore(plugins/i18n): Configure integration of vee-validate with i18n

--- plugins/i18n.ts
+++ plugins/i18n.ts
@@ -1,4 +1,6 @@
 import { createI18n } from 'vue-i18n'
+import { configure, defineRule } from 'vee-validate'
+import Rules from '@vee-validate/rules'
 import ko from '../locales/ko/ko.json'
 import en from '../locales/en/en.json'
 
@@ -11,6 +13,25 @@ export default defineNuxtPlugin(({ vueApp }) => {
     messages: {
       en,
       ko
+    }
+  })
+  Object.keys(Rules).forEach((rule) => {
+    defineRule(rule, Rules[rule])
+  })
+  defineRule('password', (value: string) => {
+    const regExp =
+      /^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{10,20}$/
+    // @ts-ignore
+    if (!regExp.test(value)) {
+      return false
+    }
+    return true
+  })
+  configure({
+    generateMessage: (context) => {
+      return i18n.global.t(`messages.${context.rule?.name}`, {
+        field: context.field
+      })
     }
   })
   vueApp.use(i18n)
